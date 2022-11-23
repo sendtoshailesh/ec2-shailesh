a4a1fb7469e2157175d569558918de64bbf03d4c
Shailesh <50418172+sendtoshailesh@users.noreply.github.com>
2022-11-23 11:28:06 +0530

    Create ec2.sh

---
+++ ec2.sh
@@ -0,0 +1,12 @@
+
+
+
+ec2 user data for web server
+
+#!/bin/bash
+yum -y install httpd
+systemctl enable httpd
+systemctl start httpd
+echo '<html><h1>Hello From Your Web Server!</h1></html>' > /var/www/html/index.html
+
+
